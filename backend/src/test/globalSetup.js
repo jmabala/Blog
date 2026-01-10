4efc674c3a9900510c78c7e7e288917043ed81c1
jmabala <38246650+jmabala@users.noreply.github.com>
2026-01-10 03:31:53 +0300

    Update globalSetup.js

--- backend/src/test/globalSetup.js
+++ backend/src/test/globalSetup.js
@@ -1,11 +1,21 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 
-export default async function globalSetup() {
-  const instance = await MongoMemoryServer.create({
-    binary: {
-      version: '6.0.6',
-    },
-  })
+// export default async function globalSetup() {
+//   const instance = await MongoMemoryServer.create({
+//     binary: {
+//       version: '6.0.6',
+//     },
+//   })
+
+// e.g., backend/src/test/globalSetup.js
+// const { MongoMemoryServer } = require('mongodb-memory-server');
+
+const mongod = await MongoMemoryServer.create({
+  binary: {
+    version: '6.0.6', // or any supported version
+  },
+});
+
   global.__MONGOINSTANCE = instance
   process.env.DATABASE_URL = instance.getUri()
 }
